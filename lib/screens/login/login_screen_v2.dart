9ab59528748edfdc5e5a22d850bef97338c57e61
nageshwar1996 <nageshwar.mondal2020@gmail.com>
2026-09-24 20:28:50 +0530

    Login page UI done

--- lib/screens/login/login_screen_v2.dart
+++ lib/screens/login/login_screen_v2.dart
@@ -23,7 +23,10 @@ class _LoginScreenV2State extends State<LoginScreenV2> {
             // (keyboard) when the user taps anywhere else on the screen
           },
           child: SingleChildScrollView(
+            // SingleChildScrollView is a widget that allows the user to scroll through a list of widgets
+            // when the content is larger than the screen
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            // use to global padding for whole screen
             child: Column(
               children: [
                 Center(child: Image.asset("assets/images/login.png")),
@@ -38,29 +41,151 @@ class _LoginScreenV2State extends State<LoginScreenV2> {
                   ),
                 ),
                 SizedBox(height: 45),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: "Enter phone number",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+                SizedBox(
+                  height: 52,
+                  child: TextField(
+                    cursorColor: const Color(0xFF009FA8),
+                    style: TextStyle(
+                      fontFamily: "Inter",
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                      color: Color(0xFF000000),
                     ),
-                    prefixIcon: DropdownButton<String>(
-                      value: '+91',
-                      items: [
-                        DropdownMenuItem(value: '+91', child: Text('+91')),
-                        DropdownMenuItem(value: '+1', child: Text('+1')),
-                      ],
-                      onChanged: (value) {
-                        print(value);
-                      },
+                    decoration: InputDecoration(
+                      hintText: "Enter phone number",
+                      hintStyle: TextStyle(
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.w400,
+                        fontSize: 18,
+                        color: Color(0xFF000000),
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 0,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFB2B2B2),
+                          width: 1,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: const BorderSide(color: Color(0xFF009FA8)),
+                      ),
+                      prefixIcon: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Color(0xFFB2B2B2),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(4),
+                          color: Color(0x33B2B2B2),
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 13,
+                          vertical: 7,
+                        ),
+                        child: DropdownButton<String>(
+                          icon: const SizedBox.shrink(),
+                          underline: SizedBox(),
+                          style: TextStyle(
+                            fontFamily: "Inter",
+                            fontWeight: FontWeight.w400,
+                            fontSize: 18,
+                            color: Color(0xFF000000),
+                          ),
+                          value: '+91',
+                          items: [
+                            DropdownMenuItem(value: '+91', child: Text('+91')),
+                            DropdownMenuItem(value: '+1', child: Text('+1')),
+                            DropdownMenuItem(value: '+12', child: Text('+12')),
+                            DropdownMenuItem(value: '+16', child: Text('+16')),
+                          ],
+                          onChanged: (value) {
+                            print(value);
+                          },
+                        ),
+                      ),
+                    ),
+                    keyboardType: TextInputType.phone,
+                  ),
+                ),
+                SizedBox(height: 35),
+                SizedBox(
+                  height: 52,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      print("Btn click");
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF009FA8),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      "Continue",
+                      style: TextStyle(
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20,
+                        color: Color(0xFFFFFFFF),
+                      ),
                     ),
                   ),
-                  keyboardType: TextInputType.phone,
-                  style: TextStyle(
-                    fontFamily: "Inter",
-                    fontWeight: FontWeight.w500,
-                    fontSize: 18,
-                    color: Color(0xFF000000),
+                ),
+                SizedBox(height: 34),
+                Center(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "New to Zydus App ?",
+                        style: TextStyle(
+                          fontFamily: "Inter",
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20,
+                          color: Color(0xFF000000),
+                        ),
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        "Sing up now",
+                        style: TextStyle(
+                          fontFamily: "Inter",
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
+                          color: Color(0xFFB355A0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 14),
+                Center(
+                  child: Text(
+                    "or sing up in with",
+                    style: TextStyle(
+                      fontFamily: "Inter",
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                      color: Color(0xFF000000),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 21),
+                Center(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset("assets/images/google.png"),
+                      SizedBox(width: 20),
+                      Image.asset("assets/images/facebook.png"),
+                    ],
                   ),
                 ),
               ],
